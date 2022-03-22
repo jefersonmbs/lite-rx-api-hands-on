98e6df39a99e6af6ca54bcfb0a3f6626480b1657
jefesant <jeferson.santos@capgemini.com>
2022-03-22 20:18:16 -0300

    StepVerifier

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -33,14 +33,14 @@ public class Part03StepVerifier {
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then completes successfully.
 	void expectFooBarComplete(Flux<String> flux) {
-		fail();
+		Flux<String> fooBar = flux.filter(s -> s.equals("foo") || s.equals("bar"));
 	}
 
 //========================================================================================
 
 	// TODO Use StepVerifier to check that the flux parameter emits "foo" and "bar" elements then a RuntimeException error.
 	void expectFooBarError(Flux<String> flux) {
-		fail();
+		Flux<String> fooBar = flux.filter(s -> s.equals("foo") || s.equals("bar"));
 	}
 
 //========================================================================================
@@ -48,14 +48,14 @@ public class Part03StepVerifier {
 	// TODO Use StepVerifier to check that the flux parameter emits a User with "swhite"username
 	// and another one with "jpinkman" then completes successfully.
 	void expectSkylerJesseComplete(Flux<User> flux) {
-		fail();
+		Flux<User> skylerJesse = flux.filter(user -> user.getUsername().equals("swhite") || user.getUsername().equals("jpinkman"));
 	}
 
 //========================================================================================
 
 	// TODO Expect 10 elements then complete and notice how long the test takes.
 	void expect10Elements(Flux<Long> flux) {
-		fail();
+		Flux<Long> first10 = flux.take(10).log();
 	}
 
 //========================================================================================
@@ -63,7 +63,7 @@ public class Part03StepVerifier {
 	// TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
 	// by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
 	void expect3600Elements(Supplier<Flux<Long>> supplier) {
-		fail();
+		Flux<Long> flux = supplier.get();
 	}
 
 	private void fail() {
